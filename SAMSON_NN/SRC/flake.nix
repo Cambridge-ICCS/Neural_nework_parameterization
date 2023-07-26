747083d973b3f148c7646060dbafa2bf74b8af00
Ben Orchard <thefirstmuffinman@gmail.com>
2023-07-26 14:39:43 +0100

    Nix flake: tweaks

--- SAMSON_NN/SRC/flake.nix
+++ SAMSON_NN/SRC/flake.nix
@@ -6,15 +6,15 @@
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
    flake-utils.lib.eachDefaultSystem (system:
      let
-       name = "samson";
-       src = ./.;
        pkgs = import nixpkgs {
          inherit system;
          #config = { allowUnfree = true; };
        };
      in {
        packages.default = pkgs.stdenv.mkDerivation {
-         inherit system name src;
+         name = "samson";
+         src = ./.;
+         #inherit system name src;
          nativeBuildInputs = with pkgs; [
            cmake
            pkgconf
@@ -28,7 +28,7 @@
          patches = [ ./cmake-gfortran.patch ];
          installPhase = ''
            mkdir -p $out/bin
-           install -m755 main $out/bin/${name}
+           install -m755 main $out/bin/samson
          '';
        };
      });
